6737348e398586ee76895f5a9a132f7b755fb932
EC2 Default User <ec2-user@ip-10-0-2-70.ap-northeast-1.compute.internal>
2022-10-20 11:53:45 +0000

    日付検索のコード移植

--- resources/js/components/DateComponent.vue
+++ resources/js/components/DateComponent.vue
@@ -20,7 +20,12 @@
               v-model="formSearch.receipt_date_start"
               locale="ja"
               :allowed-dates="allowedReceiptDateStart"
+              @input="showReceiptDateStart = false"
+              @change="handleSettingEndDate(formSearch.receipt_date_start, 'receipt_date_end')"
             ></v-date-picker>
+        <div class="custom-between">
+          〜
+        </div>
               <v-text-field
                 :value="computedReceiptDateEnd"
                 @click:clear="formSearch.receipt_date_end = null"
@@ -45,3 +50,48 @@
 </v-app>
 </div>
 </template>
+
+<script>
+import moment from 'moment';
+   export default {
+  computed: {
+    computedReceiptDateStart() {
+      return this.formSearch.receipt_date_start ? moment(this.formSearch.receipt_date_start).format('YYYY年MM月DD日') : ''
+    },
+    computedReceiptDateEnd() {
+      return this.formSearch.receipt_date_end ? moment(this.formSearch.receipt_date_end).format('YYYY年MM月DD日') : ''
+    },
+    computedCreateDateStart() {
+      return this.formSearch.create_date_start ? moment(this.formSearch.create_date_start).format('YYYY年MM月DD日') : ''
+    },
+    computedCreateDateEnd() {
+      return this.formSearch.create_date_end ? moment(this.formSearch.create_date_end).format('YYYY年MM月DD日') : ''
+    },
+    computedScheduledDateStart() {
+      return this.formSearch.scheduled_date_start ? moment(this.formSearch.scheduled_date_start).format('YYYY年MM月DD日') : ''
+    },
+    computedScheduledDateEnd() {
+      return this.formSearch.scheduled_date_end ? moment(this.formSearch.scheduled_date_end).format('YYYY年MM月DD日') : ''
+    },
+    computedAssessmentDateStart() {
+      return this.formSearch.assessment_date_start ? moment(this.formSearch.assessment_date_start).format('YYYY年MM月DD日') : ''
+    },
+    computedAssessmentDateEnd() {
+      return this.formSearch.assessment_date_end ? moment(this.formSearch.assessment_date_end).format('YYYY年MM月DD日') : ''
+    },
+    isdisabledButtonExport(){
+      let exist =[3,9].indexOf((+this.formSearch.voucher_type))===-1?false:true;
+      return exist;
+    },
+    loading(){
+      const{loadingOrderConstructionList,loadingOrderSectionList,loadingOrderCustomerList,loadingOrderSupplierList, loadingOrderAccountList, loadingOrderSubAccountList,loadingSupplierTypeListMaster,loadingGetStaffListBySectionCode}=this.loadingOver;
+      this.loadingOver.value =loadingOrderConstructionList || loadingOrderSectionList || loadingOrderCustomerList || loadingOrderSupplierList || loadingOrderAccountList || loadingOrderSubAccountList || loadingSupplierTypeListMaster || loadingGetStaffListBySectionCode;
+      this.$emit('loadingValue',this.loadingOver.value);
+      return this.loadingOver.value;
+    },
+    isDisabledButtonApproval() {
+      return this.currentRoleApprovalForOrder == 1;
+    },
+  }
+   };
+</script>
