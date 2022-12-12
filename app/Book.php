af8e50cc58599411239818eac9969d0412faee76
EC2 Default User <ec2-user@ip-10-0-2-70.ap-northeast-1.compute.internal>
2022-12-12 10:13:50 +0000

    CSVアップロード時複チェック機能修正

--- app/Book.php
+++ app/Book.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    //
+    /**
+     * CSVヘッダ項目の定義値があれば定義配列のkeyを返す   
+     *
+     * @param string $header
+     * @param string $encoding
+     * @return string|null
+     */
+    public static function retrieveTestColumnsByValue(string $header ,string $encoding)
+    {
+        // CSVヘッダとテーブルのカラムを関連付けておく
+        $list = [
+            'content' => "内容",
+            'memo'    => "備考",
+        ];
+
+        foreach ($list as $key => $value) {
+            if ($header === mb_convert_encoding($value, $encoding)) {
+                return $key;
+            }
+        }
+        return null;
+    }
 }
--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 
 //使うClassを宣言:自分で追加
@@ -303,7 +304,8 @@ public function index(Request $request)
             }
 
         }
-     $count++;
+       $error_list = [];
+        $count = 1;
 
                         //　バリデーション処理
         $validator = Validator::make($arr,[
@@ -326,9 +328,9 @@ public function index(Request $request)
             $count++;
         
         
-        if ($validator->fails()) {
+        if (count($error_list) > 0) {
            //／の画面に行きバリデーションメッセージを出す
-           return redirect('/')->withErrors($validator)->withInput()->with('message', $error_list . '件の項目を読み込みました');
+           return redirect('/')->withErrors($validator)->withInput()->with($error_list . '件の項目を読み込みました');
         }
         
         //dataに変数$arrを入れる
@@ -413,5 +415,176 @@ public function index(Request $request)
         }
         return $rules;
     }
+    
+     //・公開関数 stores(Request $request)
+    public function stores(Request $request)
+    {
+        // アップロードファイルに対してのバリデート
+        //・バリデーション = 擬似変数->validateUploadFile($request)
+        $validator = $this->validateUploadFile($request);
+        //・もしバリデーションが失敗したら
+        if ($validator->fails() === true){
+        //・ホーム画面に戻り、メッセージを表示。
+            return redirect('/form')->with('message', $validator->errors()->first('csv_file'));
+        }
+        
+        // CSVファイルをサーバーに保存
+        $temporary_csv_file = $request->file('csv_file')->store('csv');
 
+        $fp = fopen(storage_path('app/') . $temporary_csv_file, 'r');
+        //・一行目のヘッダーを読み込む
+        // 一行目（ヘッダ）読み込み
+        $headers = fgetcsv($fp);
+        //・配列column_names = [];
+        $column_names = [];
+
+        // CSVヘッダ確認
+        //・for文(変数ヘッダー as ヘッダー)
+        foreach ($headers as $header) {
+            //・変数result = テストModelのretrieveTestColumnsByValue($header,'SJIS-win');
+            $result = Test::retrieveTestColumnsByValue($header, 'SJIS-win');
+            //・もしresultがnullなら
+            if ($result === null) {
+                //・fclose($fp)
+                fclose($fp);
+                //・Storageに保存してある$temporary_csv_fileを削除
+                Storage::delete($temporary_csv_file);
+                //・ホーム画面に戻り、メッセージを表示。
+                return redirect('/form')
+                    ->with('message', '登録に失敗しました。CSVファイルのフォーマットが正しいことを確認してださい。');
+            }
+            //・$column_names[]に$resultを入れる
+            $column_names[] = $result;
+        }
+        //・registration_errors_list = [];(配列)
+        $registration_errors_list = [];
+        $update_errors_list       = [];
+        $i = 0;
+
+        // TODO:サイズが大きいCSVファイルを読み込む場合、この処理ではメモリ不足になる可能性がある為改修が必要になる
+        while ($row = fgetcsv($fp)) {
+
+            // Excelで編集されるのが多いと思うのでSJIS-win→UTF-8へエンコード
+            mb_convert_variables('UTF-8', 'SJIS-win', $row);
+            $is_registration_row = false;
+            //・for文($column_names as $column_no => $column_name)
+            foreach ($column_names as $column_no => $column_name) {
+
+                // idがなければ登録、あれば更新と判断
+                //・もしidがなかったら、登録、あれば更新削除
+                if ($column_name === 'id' && $row[$column_no] === '') {
+                    $is_registration_row = true;
+                }
+
+                // 新規登録か更新かのチェック
+                //・もしis_registration_rowがあれば
+                if($is_registration_row === true){
+                    //・もし($column_nameがかぶっていなかったら)
+                    if ($column_name !== 'id') {
+                        //・$registration_csv_id_list[$i][$column_name] = $row[$column_no] === '' ? null : $row[$column_no];
+                        $registration_csv_list[$i][$column_name] = $row[$column_no] === '' ? null : $row[$column_no];
+                    }
+                } else {
+                    $update_csv_list[$i][$column_name] = $row[$column_no] === '' ? null : $row[$column_no];
+                }
+
+            }
+
+            // バリデーションチェック
+            //・バリデーション = バリデーション作成
+            $validator = \Validator::make(
+                //・$is_registration_rowが
+                $is_registration_row === true ? $registration_csv_list[$i] : $update_csv_list[$i],
+                $this->defineValidationRules(),
+                $this->defineValidationMessages()
+            );
+            //・もしバリデーションが失敗したら、
+            if ($validator->fails() === true) {
+                //・もしis_registration_rowがあれば
+                if ($is_registration_row === true) {
+                    $registration_errors_list[$i + 2] = $validator->errors()->all();
+                } else {
+                    $update_errors_list[$i + 2] = $validator->errors()->all();
+                }
+            }
+
+            $i++;
+        }
+
+        // バリデーションエラーチェック
+        if (count($registration_errors_list) > 0 || count($update_errors_list) > 0) {
+            return redirect('/form')
+                ->with('errors', ['registration_errors' => $registration_errors_list, 'update_errors' => $update_errors_list]);
+        }
+
+        // 既存更新処理
+        if (isset($update_csv_list) === true) {
+            foreach ($update_csv_list as $update_csv) {
+                // ～更新用の処理～
+                if ($this->fill($update_csv)->save() === false) {
+                    return redirect('/form')
+                        ->with('message', '既存データの更新に失敗しました。（新規登録処理は行われずに終了しました）');
+                }
+            }
+        }
+
+        // 新規登録処理
+        if (isset($registration_csv_list) === true) {
+            foreach ($registration_csv_list as $registration_csv) {
+                // ～登録用の処理～
+                if ($this->fill($registration_csv)->save() === false) {
+                    return redirect('/form')->with('message', '新規登録処理に失敗しました。');
+                }
+            }
+        }
+
+        return redirect('/form')->with('message', 'CSV登録が完了しました。' );
+    }
+
+    /**
+     * アップロードファイルのバリデート
+     * （※本来はFormRequestClassで行うべき）
+     *
+     * @param Request $request
+     * @return Illuminate\Validation\Validator
+     */
+    private function validateUploadFile(Request $request)
+    {
+        return \Validator::make($request->all(), [
+                'csv_file' => 'required|file|mimetypes:text/plain|mimes:csv,txt',
+            ], [
+                'csv_file.required'  => 'ファイルを選択してください。',
+                'csv_file.file'      => 'ファイルアップロードに失敗しました。',
+                'csv_file.mimetypes' => 'ファイル形式が不正です。',
+                'csv_file.mimes'     => 'ファイル拡張子が異なります。',
+            ]
+        );
+    }
+
+    /**
+     * バリデーションの定義
+     *
+     * @return array
+     */
+    private function defineValidationRules()
+    {
+        return [
+            // CSVデータ用バリデーションルール
+            'content' => 'required',
+        ];
+    }
+
+    /**
+     * バリデーションメッセージの定義
+     *
+     * @return array
+     */
+    private function defineValidationMessages()
+    {
+        return [
+            // CSVデータ用バリデーションエラーメッセージ
+            'content.required' => '内容を入力してください。',
+        ];
+    }
 }
+
